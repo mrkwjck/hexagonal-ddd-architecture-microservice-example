991ebbbc514638ea89dcb34266fa306bff7d1f87
Marek Wójcik <m@rekwojcik.com>
2025-03-05 17:40:08 +0100

    Added domain service tests

--- src/test/groovy/mrkwjck/domain/AccountDomainServiceTest.groovy
+++ src/test/groovy/mrkwjck/domain/AccountDomainServiceTest.groovy
@@ -1,10 +1,33 @@
 package mrkwjck.domain
 
-
+import mrkwjck.domain.account.AccountRepository
+import mrkwjck.domain.account.event.AccountCreated
+import mrkwjck.domain.account.model.Account
 import spock.lang.Specification
 
 class AccountDomainServiceTest extends Specification {
 
+    void "should save created account and publish event for created account"() {
+        given:
+        var accountRepository = Mock(AccountRepository)
+        var domainEventPublisher = Mock(DomainEventPublisher)
+        var accountOwnerName = "Jan Kowalski"
+        var accountDomainService = new AccountDomainService(accountRepository, domainEventPublisher)
 
+        when:
+        final var account = accountDomainService.createAccount(accountOwnerName)
+
+        then:
+        account.getOwnerName() == accountOwnerName
+        1 * accountRepository.save(_) >> {
+            var savedAccount = (Account) it[0]
+            assert savedAccount.getBalance() == BigDecimal.ZERO
+            assert savedAccount.getOwnerName() == accountOwnerName
+        }
+        1 * domainEventPublisher.publishEvent(_) >> {
+            var event = (AccountCreated) it[0]
+            assert event != null
+        }
+    }
 
 }
